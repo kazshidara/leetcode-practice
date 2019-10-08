44d2fd808fc775455b364d45e3fb1be14589a35b
Kazune Shidara <kazuneshidara@gmail.com>
2019-10-08 13:06:53 -0700

    Finding disappeared numbers problem

--- arrays.py
+++ arrays.py
@@ -97,4 +97,30 @@ def get_majority(nums):
 
 ################################################################################
 
+# 6. Find all NUmbers Disappeared 
 
+# Given an array of integers where 1 ≤ a[i] ≤ n (n = size of array), some elements appear twice and others appear once.
+
+# Find all the elements of [1, n] inclusive that do not appear in this array.
+
+# Could you do it without extra space and in O(n) runtime? You may assume the returned list does not count as extra space.
+
+def findDisappearedNumbers(nums):
+    for i in range(len(nums)):
+        # print(i)
+        index = abs(nums[i]) - 1
+        # print(index)
+        nums[index] = - abs(nums[index])
+        print(nums[index])
+
+    return [i + 1 for i in range(len(nums)) if nums[i] > 0]
+
+
+
+
+
+
+
+
+
+print(findDisappearedNumbers([4,3,2,7,8,2,3,1]))
